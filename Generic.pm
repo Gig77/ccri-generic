1b8bf6e449983ceab22f8946aad7c0ded32b5aa1
Gig77 <frech.christian@gmail.com>
2013-05-31 09:45:40 +0200

    none

---
+++ Generic.pm
@@ -0,0 +1,12 @@
+package Config;
+
+use Log::Log4perl qw(:easy);
+
+# init Log4perl
+Log::Log4perl->init(\q(
+	log4perl.rootLogger = DEBUG, SCREEN
+	log4perl.appender.SCREEN = Log::Log4perl::Appender::Screen
+	log4perl.appender.SCREEN.layout = Log::Log4perl::Layout::PatternLayout
+	log4perl.appender.SCREEN.layout.ConversionPattern = %d %l %p: %m%n
+));
+our $log = Log::Log4perl->get_logger();
