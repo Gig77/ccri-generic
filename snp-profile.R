0e41529bdb5e565306ccbcf24b4ed521a5d32813
christian <frech.christian@gmail.com>
2015-03-13 12:33:28 +0200

    write file with loh/upd segments

--- snp-profile.R
+++ snp-profile.R
@@ -8,11 +8,12 @@ option_list <- list(
 		make_option("--tumor-vcf", type="character", help="VarScan2 VCF file from tumor"),
 		make_option("--normal-vcf", type="character", help="VarScan2 VCF file from matched normal"),
 		make_option("--disomic-chromosome", type="character", help="use coverage ratio of this chromosome for baseline shift (optional)"),
-		make_option("--output-file", type="character", help="name of output file")
+		make_option("--plot-output-file", type="character", help="name of output file for chromosome plots (PDF format)"),
+		make_option("--loh-segments-output-file", type="character", help="name of output file for LOH segments")
 )
 opt <- parse_args(OptionParser(option_list=option_list))
 
-#opt <- data.frame('sample-id' = "715", 'tumor-coverage' = "coverage-genome/715R.coverage-genome.tsv", 'normal-coverage' = "coverage-genome/715C.coverage-genome.tsv", 'tumor-vcf' = "varscan/715R.varscan.dbsnp.vcf", 'normal-vcf' = "varscan/715C.varscan.dbsnp.vcf", 'output-file' = "snp-profile/715R.snp-profile.pdf", stringsAsFactors=F, check.names=F)   
+#opt <- data.frame('sample-id' = "839R", 'tumor-coverage' = "coverage-genome/839R.coverage-genome.tsv", 'normal-coverage' = "coverage-genome/839C.coverage-genome.tsv", 'tumor-vcf' = "varscan/839R.varscan.dbsnp.vcf", 'normal-vcf' = "varscan/839C.varscan.dbsnp.vcf", 'plot-output-file' = "snp-profile/839R.snp-profile.pdf", 'loh-segments-output-file' = "snp-profile/839R.loh-segments.tsv", stringsAsFactors=F, check.names=F)   
 
 # read coverage data
 t.cov <- read.delim(opt$'tumor-coverage', header=F, colClasses=c("factor", "integer", "numeric"))
@@ -65,6 +66,14 @@ cna.maf <-CNA(genomdat = t.snps.het[,"maf"], chrom = t.snps.het[,"chr"], maploc 
 cna.smooth <- smooth.CNA(cna.maf)
 seg.maf <- segment(cna.smooth, alpha = 0.001, verbose=0, min.width=2)
 
+# write loh segments
+if (!is.null(opt$'loh-segments-output-file')) {
+	lohs <- seg.maf$output
+	names(lohs)[1] <- "sample"
+	lohs$sample <- opt$'sample-id'
+	write.table(lohs, file=opt$'loh-segments-output-file', row.names=F, col.names=T, sep="\t", quote=F)
+}
+
 # plot function coverage
 plot.coverage <- function(data, segs) {
 	plot(data[,"pos"], data[,"ratio"], ylim=c(-1.5,1.5), xaxt='n', yaxt='n', type="n")
@@ -98,7 +107,7 @@ plot.maf <- function(data, segs, xlim, title=title) {
 	abline(h=c(1/3, 0.25, 2/3, 0.75), lty=2)
 }
 
-pdf(opt$'output-file', width=15, height=10)
+pdf(opt$'plot-output-file', width=15, height=10)
 layout(matrix(c(1,3,5,7,9,2,4,6,8,10,11,13,15,17,19,12,14,16,18,20,21,23,25,27,29,22,24,26,28,30,31,33,35,37,39,32,34,36,38,40,41,43,45,47,49,42,44,46,48,50),10,5,byrow=T));
 par(oma=c(0, 0, 2, 0))
 for (c in c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y")) {
