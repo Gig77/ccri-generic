99ff4c47a912bec073a1cd97c9cde6fcf993df40
Gig77 <frech.christian@gmail.com>
2016-09-14 15:37:11 +0200

    none

--- make-all.sh
+++
@@ -1,5 +0,0 @@
-cd ~/modicell/results && mymake -j 20
-cd /mnt/projects/chrisi/results && mymake -j 20
-cd /mnt/projects/fikret/results && mymake -j 20
-cd /mnt/projects/iamp/results && mymake -j 20
-cd ~/sponge/results && mymake -j 20
